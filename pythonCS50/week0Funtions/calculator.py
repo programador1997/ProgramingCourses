11d563a51b148521b33effedad2fb4f9707715f0
Julio <ferraf2797@gmail.com>
2024-05-09 14:05:24 -0500

    how to use datatypes

---
+++ pythonCS50/week0Funtions/calculator.py
@@ -0,0 +1,31 @@
+
+"""
+x = input("Whats's x? ")
+y = input("Whats's y? ")
+#convert str to int
+z = int(x) + int(y)
+print(z)
+"""
+"""
+x = int(input("Whats's x? "))
+y = int(input("Whats's y? "))
+print(x+y)
+"""
+"""
+dont over complicate dont nest to much
+print(int(input("Whats's x? "))+int(input("Whats's y? ")))
+"""
+"""
+round numbers
+x = float(input("Whats's x? "))
+y = float(input("Whats's y? "))
+print(round(x+y))
+"""
+"""
+
+x = float(input("Whats's x? "))
+y = float(input("Whats's y? "))
+z = (round(x + y))
+add format to numbers add a coma
+print(f"{z:,}")
+"""
