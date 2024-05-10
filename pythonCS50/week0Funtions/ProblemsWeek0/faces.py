a7324e6d2518e318944a85cf626d0834acb21d88
Julio <ferraf2797@gmail.com>
2024-05-10 13:45:28 -0500

    impemented funtions faces.py

---
+++ pythonCS50/week0Funtions/ProblemsWeek0/faces.py
@@ -0,0 +1,12 @@
+def main():
+    text = (input())
+    convert(text)
+
+def convert(text):
+    #implementation of convertion to emoji
+    new_text = text.replace(":)", "\N{Slightly Smiling Face}")
+    final_text =new_text.replace(":(", "\N{Slightly Frowning Face}")
+    #text.replace(":(", "\N{Slightly Frowning Face}")
+    print(final_text)
+
+main()
