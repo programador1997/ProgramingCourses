d98220a0812a899f74b9aa7263767ec75510c44a
Julio de Jesus Ramirez Fernandez <rafer2797@outlook.com>
2024-05-12 07:57:31 -0600

    finished features in extension.py

--- pythonCS50/week1Conditionals/problemsWeek1/extensions.py
+++ pythonCS50/week1Conditionals/problemsWeek1/extensions.py
@@ -3,27 +3,30 @@ def main():
     checkExtension(file)
 
 def checkExtension(data):
-    last = len(data)
-    extr = last - 4
-    print(last[extr])
-    """
-    match extension:
-        case ".gif ":
-            print("image/gif")
-        case ".jpg ":
-            print("image/jpg")
-        case ".jpeg" :
-            print("image/jpeg")
-        case ".png ":
-            print("image/png")
-        case ".pdf ":
-            print("application/pdf")
-        case ".txt ":
-            print("text/plain")
-        case ".zip ":
-            print("application/zip")
-        case _:
-            print("application/octet-stream ")
+    if "." in data:
+        split_data = data.split(".")
+        second_data = split_data[1]
 
-    """
+        print(second_data)
+        match second_data:
+            case "gif":
+                print("image/gif")
+            case "jpg":
+                print("image/jpg")
+            case "jpe" :
+                print("image/jpeg")
+            case "png":
+                print("image/png")
+            case "pdf":
+                print("application/pdf")
+            case "txt":
+                print("text/plain")
+            case "zip":
+                print("application/zip")
+            case _:
+                print("application/octet-stream ")
+    else:
+        print("please introduce a file with extension")
+
+    
 main()
