c7c0195bb91f859dcfabb39b7c88c9620c453f2b
Julio <ferraf2797@gmail.com>
2024-05-21 15:54:33 -0600

    finished funtionality of plates.py

--- pythonCS50/week2Loops/problemsWeek2/plates.py
+++ pythonCS50/week2Loops/problemsWeek2/plates.py
@@ -7,51 +7,59 @@ def main():
     else:
         print("Invalid")
 
-    def is_valid(s):
-        print(f"first two: {s[0:2]}")
 
 
 
-"""
+
 
 def is_valid(s):
     i = 0
     flag_isdigit = False
-    valid = 0
+    valid = True
+    #“… vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”
+    if len(s) > 6:
+        print("longer than 7")
+        valid = False
+    if len(s) < 2:
+        print("shorter than 2")
+        valid = False
     for char in s:
         i += i
         #“All vanity plates must start with at least two letters.”
         if i == 1:
             if not char.isalpha():
-                return False
+                print("first char is not letter")
+                valid = False
         if i == 2:
             if not char.isalpha():
-                return False
+                print("second char is not letter")
+                valid = False
 
-        #“… vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”
-        if i >= 7:
-            return False
-        if i < 2:
-            return False
         #“No periods, spaces, or punctuation marks are allowed.”
         not_allowed = " .,áéíóúÁÉÍÓÚ-/_:;*-+"
         if char in not_allowed:
-            return False
+            print(f"invalid caracter: {char}")
+            valid = False
     
-        
+        """
         “Numbers cannot be used in the middle of a plate; they must come at the end.
         For example, AAA222 would be an acceptable … vanity plate;
         AAA22A would not be acceptable. The first number used cannot be a ‘0’.”
-
+        """    
         
         if flag_isdigit:
             if char.isalpha():
-                return False
+                print("a letter before a digit")
+                valid = False
             
         if char.isdigit():
             flag_isdigit = True
-        
-"""
+            if char == "0":
+                print("first digit is 0")
+                valid = False
+
+    return valid
+
     
         
 main()
