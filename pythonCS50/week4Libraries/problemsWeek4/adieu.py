52fa406259e42708c3d0ba3b0efe33e5d03d5956
Julio <ferraf2797@gmail.com>
2024-05-29 22:05:19 +0200

    started funtionality of adieu.py

---
+++ pythonCS50/week4Libraries/problemsWeek4/adieu.py
@@ -0,0 +1,21 @@
+"""
+input user name
+
+if 1 name 
+show in console
+ Adieu, adieu, to name
+if 2 names
+show in console
+ Adieu, adieu, to name and name
+if 3 or mone names
+show in console
+ Adieu, adieu, to name, name and name
+
+
+"""
+
+def main():
+    name = input("Name:")
+    ...
+
+main()
